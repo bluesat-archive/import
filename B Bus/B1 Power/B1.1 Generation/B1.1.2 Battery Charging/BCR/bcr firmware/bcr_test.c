a31278d4f7e12046e7df7bbe5960e7e3242fd8a9
tfisk <tfisk@265e18a8-33ca-405d-8757-c40a9e3cabe4>
2012-04-07 07:15:54 +0000

    M    bcr firmware/bcr.map M    bcr firmware/bcr_test.c M    bcr firmware/bcr.hex M    bcr firmware/bcr_test.h M    bcr firmware/bcr.srec M    bcr firmware/bcr.lst M    bcr firmware/bcr.elf M    bcr firmware/Makefile M    bcr firmware/bcr.bin
    git-svn-id: svn+ssh://localhost/home/pcb/svn@290 265e18a8-33ca-405d-8757-c40a9e3cabe4

--- B Bus/B1 Power/B1.1 Generation/B1.1.2 Battery Charging/BCR/bcr firmware/bcr_test.c
+++ B Bus/B1 Power/B1.1 Generation/B1.1.2 Battery Charging/BCR/bcr firmware/bcr_test.c
@@ -4,26 +4,55 @@
 #include "bcr.h"
 #include "bcr_test.h"
 
+#define MAXSTRLEN 50
+
 void testADC (void)
 {
 	uint16_t adcVals[8];
 	uint8_t i;
-	uint8_t outputString[50];
+	char outputString[MAXSTRLEN];
 
-	uart_init (9600);
+	uartInit (UART_BAUD);
 
 	// Get ADC values
 	for (i=0; i < 8; i++)
 	{
 		adcVals[i] = readADC (ADC_A, i);
 		sprintf (outputString, "ADC_A channel %d = %d\n", i, adcVals[i]);
-		uart_puts (outputString);
+		uartPuts (outputString);
 	}
 	for (i=0; i < 8; i++)
 	{
 		adcVals[i] = readADC (ADC_B, i);
 		sprintf (outputString, "ADC_B channel %d = %d\n", i, adcVals[i]);
-		uart_puts (outputString);
+		uartPuts (outputString);
 	}
 	return;
+}
+
+void uartInit (uint16_t baudRate)
+{
+	// Set baud rate
+	UBRR0H = (uint8_t) (baudRate >> 8);
+	UBRR0H = (uint8_t) baudRate;
+	// Enable rx and tx
+	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	// Set frame format: 8 data, 2 stop bits
+	UCSR0C = (1 << USBS0) | (3 << UCSZ00);
+}
+
+void uartTransmit (uint8_t data)
+{
+	while ( !(UCSR0A & (1 << UDRE0) ))
+			;
+
+	// Put data in buffer
+	UDR0 = data;
+}
+
+void uartPuts (char* string)
+{
+	uint8_t i;
+	for(i=0; i < MAXSTRLEN && string[i]!='\0'; i++)
+		uartTransmit ((uint8_t)string[i]);
 }
--- B Bus/B1 Power/B1.1 Generation/B1.1.2 Battery Charging/BCR/bcr firmware/bcr_test.h
+++ B Bus/B1 Power/B1.1 Generation/B1.1.2 Battery Charging/BCR/bcr firmware/bcr_test.h
@@ -8,4 +8,12 @@
 //	 bcr_test.h : Allows testing of BCR over UART serial interface
 //
 
+#define UART_BAUD 416
+
 void testADC (void);
+
+void uartInit (uint16_t baudRate);
+
+void uartTransmit (uint8_t data);
+
+void uartPuts (char* string);
